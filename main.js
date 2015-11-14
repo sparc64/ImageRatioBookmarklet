d3b6eddaf4db1147db2a45a52a7cfd240bc11ccc
sparc64 <sparc64@users.noreply.github.com>
2015-11-14 21:47:36 +0000

    basic functional

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
 javascript:(function(){
+	var image = document.images[0];
 	
-})()
+	var TARGET_RATIO = { horizontal: 0.6, vertical: 1.5 };
+	
+	
+	if (image.complete) {
+		var realHeight = image.naturalHeight;
+		var	realWidth = image.naturalWidth;
+		var realRatio = realWidth / realHeight;
+		
+		if ( realRatio === TARGET_RATIO[horizontal] || realRatio === TARGET_RATIO[vertical] ) {
+			alert("PASS");
+		} else {
+			alert("FAIL")
+		}
+	} esle {
+		console.log("Image error");	
+	}
+})();
